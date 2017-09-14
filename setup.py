386528e20fb4d951cce71d0e798aa6e9dd4d5c00
Vinod Kumar <kuvinod@microsoft.com>
2017-09-14 15:54:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 
 from setuptools import setup, find_packages
 
-NAME = "vsts-cd-manager-demo"
+NAME = "vsts-cd-manager-demo-kuvinod-test"
 VERSION = "0.1.0"
 
 # To install the library, run the following
